38ffd75c08378f773a03ef9af04bbfc993af6d1b
Angus Hardie <angus.hardie@malcolmhardie.com>
2011-10-24 09:36:36 +0100

    misc build files

---
+++ prefix-header.h
@@ -0,0 +1,12 @@
+/*
+ *  prefix-header.h
+ *  TesseractUB
+ *
+ *  Created by Angus W Hardie on 10/05/2008.
+ *  Copyright 2008 MalcolmHardie Solutions Ltd. All rights reserved.
+ *
+ */
+
+#define HAVE_CONFIG_H 1
+
+#define GRAPHICS_DISABLED 1
